62fab6d39b9ff88232b4d4c8884b36a3e1277b78
lazy-mannn <160388667+lazy-mannn@users.noreply.github.com>
2024-03-15 12:37:57 +0200

    Changed RFID+LCD's functionality and name to RFID+LCD+RTC,and added included libraies

--- RFID_RTC_LCDv2.ino
+++ RFID_RTC_LCDv2.ino
@@ -2,7 +2,13 @@
 #include <MFRC522.h>
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
+#include "RTClib.h"
 
+
+
+RTC_DS1307 RTC;
+
+/*
 byte D[8] = {
 	0b01110,
 	0b01010,
@@ -53,8 +59,8 @@ byte SH[8] = {
 	0b11111,
 	0b00000
 };
-
-
+//БГ букви
+*/
 #define gr_pin 8
 #define red_pin 7 
 
@@ -75,17 +81,31 @@ void setup()
   pinMode(red_pin, OUTPUT);
   lcd.begin();
   lcd.backlight();
+
+  RTC.begin(); // load the time from your computer.
+
+if (! RTC.isrunning())
+
+{
+
+Serial.println("RTC is NOT running!");// This will reflect the time that your sketch was compiled
+
+RTC.adjust(DateTime(__DATE__, __TIME__));
   
 
-
+  /*
   lcd.createChar(0, Z);
   lcd.createChar(1, D);
   lcd.createChar(2, U);
   lcd.createChar(3, P);
   lcd.createChar(4, SH);
+  */
+}
 }
 void loop() 
 {
+  lcd.clear();
+  DateTime now = RTC.now();
   // Look for new cards
   if ( ! mfrc522.PICC_IsNewCardPresent()) 
   {
@@ -115,7 +135,9 @@ void loop()
     Serial.println("Authorized access");
     Serial.println();
     digitalWrite(gr_pin, HIGH);
-    
+    lcd.print("Checked in");
+    cycle();
+  /*  
     lcd.setCursor(0, 0);
     lcd.print("PA");
     lcd.setCursor(2, 0);
@@ -134,7 +156,7 @@ void loop()
     lcd.write(byte(2));
     lcd.setCursor(14, 0);
     lcd.write(byte(3));
-    
+  */
     
     delay(5000);
     lcd.clear();
@@ -144,8 +166,9 @@ void loop()
  else   {
     Serial.println("Access denied");
     digitalWrite(red_pin, HIGH);
-    lcd.print("");\
-
+    lcd.print(" Try again");
+    cycle();
+/*
     lcd.setCursor(0, 0);
     lcd.write(byte(1));
     lcd.setCursor(1, 0);
@@ -160,11 +183,77 @@ void loop()
     lcd.write(byte(0));
     lcd.setCursor(12, 0);
     lcd.print("AH");
-
+*/
 
     delay(5000);
     digitalWrite(red_pin, LOW);
     lcd.clear();
     
       }
+  
 } 
+void cycle(){
+  DateTime now = RTC.now();
+lcd.setCursor(0, 1);
+lcd.print(now.month(), DEC);
+
+lcd.print('/');
+
+lcd.print(now.day(), DEC);
+
+lcd.print(' ');
+
+lcd.print(now.hour(), DEC);
+Serial.print(now.hour(), DEC);
+lcd.print(':');
+Serial.print(":");
+lcd.print(now.minute(), DEC);
+Serial.print(now.minute(), DEC);
+lcd.print(':');
+Serial.print(":");
+lcd.print(now.second(), DEC);
+Serial.print(now.second(), DEC);
+uint16_t year = now.year();
+uint8_t month = now.month();
+uint8_t day = now.day();
+Serial.print(" ");
+switch(weekday(year,month,day)){
+     case 1:
+     lcd.print("Mon");
+     break;
+     case 2:
+     lcd.print("Tue");
+     break;
+     case 3:
+     lcd.print("Wed");
+     break;
+     case 4:
+     lcd.print("Thu");
+     break;
+     case 5:
+     lcd.print("Fri");
+     break;
+     case 6:
+     lcd.print("Sat");
+     break;
+     case 7:
+     lcd.print("Sun");
+     
+     break;
+     default: lcd.print("Day error!"); break;
+     }
+     
+
+
+}
+int weekday(int year,int month,int day)
+/* Calculate day of week in proleptic Gregorian calendar. Sunday == 0. */
+{
+  int adjustment, mm, yy;
+  if (year<2000) year+=2000;
+  adjustment = (14 - month) / 12;
+  mm = month + 12 * adjustment - 2;
+  yy = year - adjustment;
+  return (day + (13 * mm - 1) / 5 +
+    yy + yy / 4 - yy / 100 + yy / 400) % 7;
+}  
